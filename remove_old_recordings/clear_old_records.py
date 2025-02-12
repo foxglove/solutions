e09f9413d20cebecbd9bb0e60f4eda48c536bd46
Mike Moore <mike@themsquared.com>
2025-02-12 10:28:51 -0800

    Delete old recordings.

--- remove_old_recordings/clear_old_records.py
+++ remove_old_recordings/clear_old_records.py
@@ -1,46 +1,43 @@
 import os
 import csv
 from dotenv import load_dotenv
+from datetime import datetime, timedelta, timezone
 load_dotenv()
 
 from foxglove_data_platform.client import Client
 
 token = os.getenv("FOXGLOVE_API_KEY")
+days_to_keep = os.getenv("DAYS_TO_KEEP")
+delete_limit = os.getenv("DELETE_LIMIT")
+
+latest_date = datetime.now(timezone.utc) - timedelta(days=int(days_to_keep))
+print(latest_date)
+
+if(token is None):
+    raise ValueError("FOXGLOVE_API_KEY is not set")
+
+if(days_to_keep is None):
+    raise ValueError("DAYS_TO_KEEP is not set")
+
 client = Client(token=token)
 
 recordings = client.get_recordings()
-devices = client.get_devices()
 
-# Build the report for devices and recordings per device
-report = {
-    "devices": [{
-        "name": device["name"],
-        "id": device["id"],
-        "recordings": [],
-        "total_size": 0
-    } for device in devices]
-}
-report["devices"].append({
-    "name": "No Device",
-    "id": "no_device",
-    "recordings": [],
-    "total_size": 0
-})
+# Delete a recording
+def delete_recording(recording_id):
+    client.delete_recording(recording_id=recording_id)
 
+# List and delete recordings
+delete_count = 0
 for recording in recordings:
-    if(recording["device"] is None):
-        for device in report["devices"]:
-            if(device["id"] == "no_device"):
-                device["recordings"].append(recording["id"])
-                device["total_size"] += recording["size"]
-    else:
-        for device in report["devices"]:
-            if(device["id"] == recording["device"]["id"]):
-                device["recordings"].append(recording["id"])
-                device["total_size"] += recording["size"]
+    if recording["created_at"] < latest_date:
+        if recording["device"] is not None:
+            device_id = recording["device"]["name"]
+        else:
+            device_id = "No Device"
+        print(f"Deleting recording {recording['id']} from {recording['created_at']} on device {device_id}")
+        delete_recording(recording["id"])
+        delete_count += 1
+        if delete_count >= int(delete_limit) and int(delete_limit) != 0:
+            break
 
-with open("report.csv", "w") as f:
-    writer = csv.writer(f)
-    writer.writerow(["Device", "Recording Count", "Total Size"])
-    for device in report["devices"]:
-        writer.writerow([device["name"], len(device["recordings"]), device["total_size"]])
